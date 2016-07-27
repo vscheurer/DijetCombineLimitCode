b973372835ecc92920cd5b67b32defa5ab816829
thaarres <thea.aarrestad@cern.ch>
2016-07-27 14:01:30 +0200

    removed unneccessary files

--- ProduceWorkspaces13TeV.py
+++ ProduceWorkspaces13TeV.py
@@ -1,104 +1,17 @@
-import os
-masses =[m*100 for m in range(12,40+1)]
-masses =[1000,1200,1400,1800,2000,2500,3000,3500,4000]
-masses =[1000,1200,1400,1800,2000,2500,3000,3500,4000]
-for mass in masses:
-  outputname = "submit_HVT_"+str(mass)+".src"
-  logname = "submit_HVT_"+str(mass)+".log"
-  outputfile = open(outputname,'w')
-  outputfile.write('#!/bin/bash\n')
-  outputfile.write("cd ${CMSSW_BASE}/src/DijetCombineLimitCode; eval `scramv1 run -sh`\n")
-  outputfile.write("root -b -q 'R2JJFitter13TeV.cc("+str(mass)+","+'""'+",2,1)'\n")
-  outputfile.close()
-
-  command="rm "+logname
-  print command
-  os.system(command)
-  #command="bsub -q 1nh -o "+logname+" source "+outputname
-  command="chmod 755 ./"+outputname+";./"+outputname
-  print command
-  os.system(command)
-
+import os,sys
+postfix = "sideband_"
 # masses =[m*100 for m in range(12,40+1)]
-# masses=[2000]
-# for mass in masses:
-#   outputname = "submit_Wprime_"+str(mass)+".src"
-#   logname = "submit_Wprime_"+str(mass)+".log"
-#   outputfile = open(outputname,'w')
-#   outputfile.write('#!/bin/bash\n')
-#   outputfile.write("cd ${CMSSW_BASE}/src/DijetCombineLimitCode; eval `scramv1 run -sh`\n")
-#   outputfile.write("root -b -q 'R2JJFitter13TeV.cc("+str(mass)+","+'""'+",2)'\n")
-#   outputfile.close()
-#   command="rm "+logname
-#   print command
-#   os.system(command)
-#   #command="bsub -q 1nh -o "+logname+" source "+outputname
-#   command="chmod 755 ./"+outputname+";./"+outputname
-#   print command
-#   os.system(command)
-
-#masses =[m*100 for m in range(12,40+1)]
+masses =[1000,1200,1400,1800,2000,2500,3000,3500,4000]
+# masses = [2000,3000]
+samples = [2]  # 0==RS1WW/ZZ 2==W'/Z' 4==BulkG WW/ZZ else==qW/qZ
 for mass in masses:
-  outputname = "submit_BulkVV_"+str(mass)+".src"
-  logname = "submit_BulkVV_"+str(mass)+".log"
-  outputfile = open(outputname,'w')
-  outputfile.write('#!/bin/bash\n')
-  outputfile.write("cd ${CMSSW_BASE}/src/DijetCombineLimitCode; eval `scramv1 run -sh`\n")
-  outputfile.write("root -b -q 'R2JJFitter13TeV.cc("+str(mass)+","+'""'+",4,1)'\n")
-  outputfile.close()
-
-  command="rm "+logname
-  print command
-  os.system(command)
-  #command="bsub -q 1nh -o "+logname+" source "+outputname
-  command="chmod 755 ./"+outputname+";./"+outputname
-  print command
-  os.system(command)
-
-# masses =[m*100 for m in range(10,40+1)]
-# masses = []
-#
-# for mass in masses:
-#   outputname = "submit_RS1VV_"+str(mass)+".src"
-#   logname = "submit_RS1VV_"+str(mass)+".log"
-#   outputfile = open(outputname,'w')
-#   outputfile.write('#!/bin/bash\n')
-#   outputfile.write("cd ${CMSSW_BASE}/src/DijetCombineLimitCode; eval `scramv1 run -sh`\n")
-#   outputfile.write("root -b -q 'R2JJFitter13TeV.cc("+str(mass)+","+'""'+",0)'\n")
-#   outputfile.close()
-#
-#   command="rm "+logname
-#   print command
-#   os.system(command)
-#   #command="bsub -q 1nh -o "+logname+" source "+outputname
-#   command="chmod 755 ./"+outputname+";./"+outputname
-#   print command
-#   os.system(command)
-#
-#
-# masses =[m*100 for m in range(10,60+1)]
-# masses = []
-#
-# for mass in masses:
-#   outputname = "submit_qV_"+str(mass)+".src"
-#   logname = "submit_qV_"+str(mass)+".log"
-#   outputfile = open(outputname,'w')
-#   outputfile.write('#!/bin/bash\n')
-#   outputfile.write("cd ${CMSSW_BASE}/src/DijetCombineLimitCode; eval `scramv1 run -sh`\n")
-#   outputfile.write("root -b -q 'R2JJFitter13TeV.cc("+str(mass)+","+'""'+",1)'\n")
-#   outputfile.close()
-#
-#   command="rm "+logname
-#   print command
-#   os.system(command)
-#   #command="bsub -q 1nh -o "+logname+" source "+outputname
-#   command="chmod 755 ./"+outputname+";./"+outputname
-#   print command
-#   os.system(command)
-#
-# masses =[m*100 for m in range(10,40+1)]
-# masses = []
-#
+  for sample in samples:
+    channel = 1    # 1==VV 2==qV 3==No purity
+    if sample != 0 and sample != 2 and sample != 4: 
+      channel = 2
+    command = 'qsub -q short.q submitWorkspaceJobs.sh %i %i %i %s'%(mass,sample,channel,postfix)
+    print command
+    os.system(command)
 
 
 
